4d7002fadc06f38b5402d613a6e55e726a442908
KatOli4ka <katoli4ka87@mail.ru>
2022-11-10 17:35:52 +0500

    ДЗ 2, задача 1

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,10 @@ for (int i=0; i<=17; i++){if (i%2==0){
 
 for (int i=10; i>=-10;i--){
     System.out.println("Итерация цикла "+i);}
+
+for(int i=1904; i<2096;i=i+4){
+    System.out.println(i+" год является високосным");}
+
+
             }
 }
