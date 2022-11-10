09b1b697e17de42ab183a8b6a59dc2f18f0ca633
KatOli4ka <katoli4ka87@mail.ru>
2022-11-10 17:58:06 +0500

    ДЗ 3, задача 2

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,16 @@ int deposit=29000;
 int total=0;
 for (int i=0;i<12;i++){total=total+deposit;
     System.out.println("Месяц "+i+", сумма накоплений равна "+total+" рублей");}
+
+int deposit1=29000;
+int total1=0;
+for (int i=0;i<12;i++){
+    total1=total1+total1/100;
+    total1=total1+deposit1;
+    System.out.println("Месяц "+i+", сумма накоплений равна "+total1+" рублей");
+}
+
+
+
             }
 }
