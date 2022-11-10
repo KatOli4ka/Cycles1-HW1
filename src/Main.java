ec43674ee0b8b9fa471b63d4c1d5d05cfa0b9cc5
KatOli4ka <katoli4ka87@mail.ru>
2022-11-10 15:33:33 +0500

    ДЗ 1, задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 for (int i=0; i<=10; i++){
     System.out.println("Итерация цикла "+i);
 }
+for (int i=10; i>=0; i--){
+    System.out.println("Итерация цикла "+i);}
             }
 }
