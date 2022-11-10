4c498262195c3d7001d09c896ab973d7a6d5912d
KatOli4ka <katoli4ka87@mail.ru>
2022-11-10 17:29:15 +0500

    ДЗ 1, задача 4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,8 @@ for (int i=10; i>=0; i--){
     System.out.println("Итерация цикла "+i);}
 for (int i=0; i<=17; i++){if (i%2==0){
     System.out.println("Четные числа "+i);}}
+
+for (int i=10; i>=-10;i--){
+    System.out.println("Итерация цикла "+i);}
             }
 }
