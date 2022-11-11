45ea95277fa56a7bd0eb8bea25fe4543bbe03308
KatOli4ka <katoli4ka87@mail.ru>
2022-11-11 17:52:13 +0500

    ДЗ 3, корректировка + создание пулл реквеста

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,48 @@
 public class Main {
     public static void main(String[] args) {
 
-for (int i=0; i<=10; i++){
+        System.out.println("Задание 1");
+for (int i=1; i<=10; i++){
     System.out.println("Итерация цикла "+i);
 }
-for (int i=10; i>=0; i--){
+        System.out.println("Задание 2");
+for (int i=10; i>=1; i--){
     System.out.println("Итерация цикла "+i);}
+
+        System.out.println("Задание 3");
 for (int i=0; i<=17; i++){if (i%2==0){
     System.out.println("Четные числа "+i);}}
 
+        System.out.println("Задание 4");
 for (int i=10; i>=-10;i--){
     System.out.println("Итерация цикла "+i);}
 
-for(int i=1904; i<2096;i=i+4){
+        System.out.println("ДЗ-2. Задание 1");
+for(int i=1904; i<2096;i=i+4){if(i%100!=0||i%400==0)
     System.out.println(i+" год является високосным");}
 
+        System.out.println("ДЗ-2. Задание 2");
 for (int i=7;i<=98;i=i+7){
-    System.out.println(i);}
+    System.out.print(i+" ");
+}
+        System.out.println();
 
+        System.out.println("ДЗ-2. Задание 3");
 for (int i=1;i<=512;i=i*2){
-    System.out.println(i);}
+    System.out.print(i+" ");}
+        System.out.println();
 
-
+        System.out.println("ДЗ-3. Задание 1");
 int deposit=29000;
 int total=0;
-for (int i=0;i<=12;i++){total=total+deposit;
+for (int i=1;i<=12;i++){total=total+deposit;
     System.out.println("Месяц "+i+", сумма накоплений равна "+total+" рублей");}
 
-        int deposit1=29000;
-        int total1=0;
-        for (int i=0;i<=12;i++){
-            total1=total1+total1/100;
-            total1=total1+deposit1;
+        System.out.println("ДЗ-3.Задание 2");
+        double percent=0.01;
+        double total1=0;
+        for (int i=1;i<=12;i++){
+           total1=(total1+deposit)*(1+percent);
             System.out.println("Месяц "+i+", сумма накоплений равна "+total1+" рублей");
         }
 
